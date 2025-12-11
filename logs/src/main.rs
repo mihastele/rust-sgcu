9e7a60e6988edd111e57527ec664e98e2531db6f
Miha <stele1000@gmail.com>
2025-12-11 15:43:05 +0100

    Mutability solved (but gets copied)

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -2,14 +2,14 @@ use std::fs;
 use std::io::Error;
 
 
-fn extract_errors(text: &str) -> Vec<&str> {
+fn extract_errors(text: &str) -> Vec<String> {
     let split_text = text.split("\n");
 
     let mut results = vec![];
 
     for line in split_text {
-        if line.starts_with("Error") {
-            results.push(line);
+        if line.starts_with("ERROR") {
+            results.push(line.to_string());
         }
     }
 
@@ -28,11 +28,15 @@ fn main() {
     //     Err(what_went_wrong) => println!("Error: {}", what_went_wrong),
     // }
 
-    match fs::read_to_string("logs.txt") {
-        Ok(contents) => println!("Contents: {}", contents),
-        Err(why) => println!("Couldn't read file: {}", why),
-    }
+    let mut error_logs = vec![];
 
+    match fs::read_to_string("logs.txt") {
+        Ok(contents) => {
+            error_logs = extract_errors(contents.as_str());
+        }
+        Err(why) => println!("Couldn't read file: {}", why)
+    }
+    println!("Found {} errors\n{:#?}", error_logs.len(), error_logs);
 }
 
 // fn validate_email(email: &str) -> Result<(), Error> {
