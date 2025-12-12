1200e0ca611bc6138c0969679ef78c9c8b4c521e
Miha <stele1000@gmail.com>
2025-12-12 18:41:02 +0100

    WIP

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,6 +1,15 @@
+use num_traits::ToPrimitive;
+
+
+// First version
+
+
+// Second - any kind of number
 
 fn pythagoras(a: f64, b: f64) -> f64 {
-    (a.powi(2) + b.powi(2)).sqrt()
+    let a_f64 = a.to_f64().unwrap(); // included in num_traits
+    let b_f64 = b.to_f64().unwrap(); // included in num_traits
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
 
 fn main() {
@@ -9,8 +18,10 @@ fn main() {
     let b: f64 = 4.0;
     let c: i32 = 5;
 
-    a + b + c; // No arithmetic allowed between different types
+    // a + b + c; // No arithmetic allowed between different types
+    let a_f64: f64 = a as f64;
+    let a_f64 = a.to_f64().unwrap(); // included in num_traits
 
     println!("{}", pythagoras(3.0, 4.0));
-    println!("{}", pythagoras(a, b));
+    println!("{}", pythagoras(a_f64, b));
 }
