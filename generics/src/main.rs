3aa6a12923b091f538558e3b5d618a483a216241
Miha <stele1000@gmail.com>
2025-12-12 19:06:31 +0100

    Any num type combination pythagoras

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,23 +1,24 @@
-use num_traits::{Float, ToPrimitive};
+use num_traits::ToPrimitive;
 
-
-// First version
-
+// // First version
+// fn pythagoras<T: Float, U: Float>(a: T, b: U) -> f64 {
+//     let a_f64 = a.to_f64().unwrap(); // included in num_traits
+//     let b_f64 = b.to_f64().unwrap(); // included in num_traits
+//     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+// }
 
 // Second - any kind of number
-
-fn pythagoras<T: Float, U: Float>(a: T, b: U) -> f64 {
+fn pythagoras<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
     let a_f64 = a.to_f64().unwrap(); // included in num_traits
     let b_f64 = b.to_f64().unwrap(); // included in num_traits
     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
 
 fn main() {
-
     let a: f32 = 3.0;
     let b: f64 = 4.0;
     let c: i32 = 5;
 
     // println!("{}", pythagoras(3.0, 4.0));
-    println!("{}", pythagoras::<f32,f64>(a, b));
+    println!("{}", pythagoras::<f32, f64>(a, b));
 }
