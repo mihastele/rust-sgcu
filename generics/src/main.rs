18bf9f9436b19a34ab3b95a4bb468b557d4b4dfd
Miha <stele1000@gmail.com>
2025-12-12 18:34:47 +0100

    Not allowed

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,16 @@
+
+fn pythagoras(a: f64, b: f64) -> f64 {
+    (a.powi(2) + b.powi(2)).sqrt()
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let a: f32 = 3.0;
+    let b: f64 = 4.0;
+    let c: i32 = 5;
+
+    a + b + c; // No arithmetic allowed between different types
+
+    println!("{}", pythagoras(3.0, 4.0));
+    println!("{}", pythagoras(a, b));
 }
